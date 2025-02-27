17e1bf68f28698cfc46d4951035c1d483e84e482
kapil <kapilprajapat8130@gmail.com>
2025-02-27 23:27:32 +0530

    1st

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page! 
+welcome! I love my india and also love ducat and 
+</h1>
+
+<?php
+echo "Hello World! Ashi!!!!! i am adityaa from ducat and also from india 
+saasddafafda
+afafafeaf";
+?>
+
+</body>
+</html>
